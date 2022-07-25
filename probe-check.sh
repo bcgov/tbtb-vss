c34fe7810652bb3b5af0a2270c000ef616edeac3
Marsh, Kal AEST:EX <kal.marsh@gov.bc.ca>
2022-07-25 14:28:26 -0700

    second commit

--- probe-check.sh
+++ probe-check.sh
@@ -1,17 +1,8 @@
 #!/bin/sh
 
-if [ $(php /var/www/html/artisan dusk:chrome-driver --detect | grep -v grep | grep -c Exception) -ge 1 ]; then
-  exit 1
-else
-    ### Check if queue died ###
-    if [ $(ps -aux | grep queue | grep -v grep | grep -c queue:listen) -eq 0 ]; then
-      exit 1
-    fi
-
-    ### Check if vendor directory does not exist ###
-    ### composer did not install ###
-    if [ ! -d "/var/www/html/vendor" ]; then
-        exit 1
-    fi
-  exit 0
+### Check if vendor directory does not exist ###
+### composer did not install ###
+if [ ! -d "/var/www/html/vendor" ]; then
+    exit 1
 fi
+exit 0
