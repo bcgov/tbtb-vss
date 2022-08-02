71f069473db1c8eab95bfa32647bd6279a908ab9
Marsh, Kal AEST:EX <kal.marsh@gov.bc.ca>
2022-08-02 12:44:46 -0700

    adds reports export

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -27,7 +27,7 @@ class ReportController extends Controller
     public function searchReports(Request $request)
     {
         list($results['pre'], $results['post'], $results['total']) = $this->fetchReport($request);
-        return Inertia::render('Reports', ['results' => $results]);
+        return Inertia::render('Reports', ['results' => $results, 'start' => $request->inputStartDate, 'end' => $request->inputEndDate]);
     }
 
     private function fetchReport(Request $request)
@@ -41,6 +41,7 @@ class ReportController extends Controller
             foreach ($funding_types as $type){
                 $table[$area->description][$type->funding_type] = 0;
             }
+            asort($table[$area->description]);
         }
         $pre_audit_table = $table;
         $post_audit_table = $table;
@@ -92,90 +93,4 @@ class ReportController extends Controller
 //        echo "</pre></td></tr></table>";
    }
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Inertia\Response::render
-     */
-    public function index()
-    {
-        return Inertia::render('Dashboard');
-    }
-
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Inertia\Response::render
-     */
-    public function goToPage(Request $request, $page = 'areas-of-audit')
-    {
-        return Inertia::render('Maintenance', ['page' => $page]);
-    }
-
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\AreaOfAudit  $areaOfAudit
-     * @return \Illuminate\Http\Response
-     */
-    public function show(AreaOfAudit $areaOfAudit)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\AreaOfAudit  $areaOfAudit
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(AreaOfAudit $areaOfAudit)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\AreaOfAudit  $areaOfAudit
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, AreaOfAudit $areaOfAudit)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\AreaOfAudit  $areaOfAudit
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(AreaOfAudit $areaOfAudit)
-    {
-        //
-    }
 }
--- resources/js/Pages/Reports.vue
+++ resources/js/Pages/Reports.vue
@@ -61,15 +61,44 @@
                     <div class="col-md-8 mt-3">
                         <div class="card mb-3">
                             <div class="card-header">
-                                VSS Reports
+                                VSS Report <template v-if="results != null"><small class="text-muted">from {{start}} to {{end}}</small></template>
+
+                                <div v-if="results != null" class="btn-group float-end" role="group" aria-label="Reports view">
+                                    <button @click="switchView('chart')" type="button" class="btn btn-sm btn-outline-primary">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bar-chart-line" viewBox="0 0 16 16">
+                                            <path d="M11 2a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v12h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h1V7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7h1V2zm1 12h2V2h-2v12zm-3 0V7H7v7h2zm-5 0v-3H2v3h2z"/>
+                                        </svg>
+                                    </button>
+                                    <button @click="switchView('grid')" type="button" class="btn btn-sm btn-outline-primary">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-grid-3x3" viewBox="0 0 16 16">
+                                            <path d="M0 1.5A1.5 1.5 0 0 1 1.5 0h13A1.5 1.5 0 0 1 16 1.5v13a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 14.5v-13zM1.5 1a.5.5 0 0 0-.5.5V5h4V1H1.5zM5 6H1v4h4V6zm1 4h4V6H6v4zm-1 1H1v3.5a.5.5 0 0 0 .5.5H5v-4zm1 0v4h4v-4H6zm5 0v4h3.5a.5.5 0 0 0 .5-.5V11h-4zm0-1h4V6h-4v4zm0-5h4V1.5a.5.5 0 0 0-.5-.5H11v4zm-1 0V1H6v4h4z"/>
+                                        </svg>
+                                    </button>
+                                    <button @click="switchView('download')" type="button" class="btn btn-sm btn-outline-primary">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-down" viewBox="0 0 16 16">
+                                            <path fill-rule="evenodd" d="M3.5 10a.5.5 0 0 1-.5-.5v-8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 0 0 1h2A1.5 1.5 0 0 0 14 9.5v-8A1.5 1.5 0 0 0 12.5 0h-9A1.5 1.5 0 0 0 2 1.5v8A1.5 1.5 0 0 0 3.5 11h2a.5.5 0 0 0 0-1h-2z"/>
+                                            <path fill-rule="evenodd" d="M7.646 15.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 14.293V5.5a.5.5 0 0 0-1 0v8.793l-2.146-2.147a.5.5 0 0 0-.708.708l3 3z"/>
+                                        </svg>
+                                    </button>
+                                </div>
                             </div>
                             <div class="card-body">
                                 <div v-if="results != null" class="table-responsive pb-3">
-                                    <ReportChart :results="results.pre" title="Pre Audit Report" bg="#0d6efd" />
-                                    <hr/>
-                                    <ReportChart :results="results.post" title="Post Audit Report" bg="#198754" />
-                                    <hr/>
-                                    <ReportChart :results="results.total" title="Totals Report" bg="#0dcaf0" />
+                                    <template v-if="currentView === 'chart'">
+                                        <ReportChart :results="results.pre" title="Pre Audit Report" bg="#0d6efd" />
+                                        <hr/>
+                                        <ReportChart :results="results.post" title="Post Audit Report" bg="#198754" />
+                                        <hr/>
+                                        <ReportChart :results="results.total" title="Totals Report" bg="#0dcaf0" />
+                                    </template>
+                                    <template v-else>
+                                        <ReportGrid :results="results.pre" title="Pre Report" bg="#0dcaf0" tableId="reports_pre" />
+                                        <hr/>
+                                        <ReportGrid :results="results.post" title="Post Report" bg="#0dcaf0" tableId="reports_post" />
+                                        <hr/>
+                                        <ReportGrid :results="results.total" title="Totals Report" bg="#0dcaf0" tableId="reports_total" />
+                                        <hr/>
+                                    </template>
                                 </div>
                                 <h1 v-else class="lead">No results</h1>
                             </div>
@@ -90,28 +119,44 @@ import BreezeLabel from "@/Components/Label";
 import BreezePagination from "@/Components/Pagination";
 import BreezeButton from "@/Components/Button";
 import ReportChart from "@/Components/ReportChart";
+import ReportGrid from "@/Components/ReportGrid";
 
 export default {
     name: 'Reports',
     components: {
-        BreezeAuthenticatedLayout, Head, Link, BreezeInput, BreezeSelect, BreezeLabel, BreezePagination, BreezeButton, ReportChart
+        BreezeAuthenticatedLayout, Head, Link, BreezeInput, BreezeSelect, BreezeLabel, BreezePagination, BreezeButton, ReportChart, ReportGrid
     },
     props: {
-        results: Object
+        results: Object,
+        start: String,
+        end: String
     },
     data() {
         return {
             reportForm: {
-                inputEndDate: '',
-                inputStartDate: '',
+                inputEndDate: '2022-08-04',
+                inputStartDate: '1990-08-04',
                 type: 'prevented',
             },
-            //results: null,
+            currentView: 'chart',
             showSuccessMsg: false,
 
         }
     },
     methods: {
+        switchView: function (viewType)
+        {
+            this.currentView = viewType;
+            if(viewType === 'download'){
+                this.currentView = 'grid';
+                let vm = this;
+                setTimeout(function (){
+                    vm.downloadTableAsCsv('reports_pre');
+                    vm.downloadTableAsCsv('reports_post');
+                    vm.downloadTableAsCsv('reports_total');
+                }, 1000);
+            }
+        },
         changeType: function (type){
             this.reportForm.type = type;
         },
@@ -127,23 +172,49 @@ export default {
                 },
                 preserveState: false
             });
-
         },
-        showSuccessAlert: function ()
-        {
-            this.showSuccessMsg = true;
-            let vm = this;
-            setTimeout(function (){
-                vm.showSuccessMsg = false;
-                vm.noChanges = true;
-            }, 5000);
-        },
+        // Quick and simple export target #table_id into a csv
+        downloadTableAsCsv: function(table_id, separator = ',') {
+            // Select rows from table_id
+            var rows = document.querySelectorAll('table#' + table_id + ' tr');
+            // Construct csv
+            var csv = [];
+            for (var i = 0; i < rows.length; i++) {
+                var row = [], cols = rows[i].querySelectorAll('td, th');
+                for (var j = 0; j < cols.length; j++) {
+                    // Clean innertext to remove multiple spaces and jumpline (break csv)
+                    var data = cols[j].innerText.replace(/(\r\n|\n|\r)/gm, '').replace(/(\s\s)/gm, ' ')
+                    // Escape double-quote with double-double-quote (see https://stackoverflow.com/questions/17808511/properly-escape-a-double-quote-in-csv)
+                    data = data.replace(/"/g, '""');
+                    // Push escaped string
+                    row.push('"' + data + '"');
+                }
+                csv.push(row.join(separator));
+            }
+            var csv_string = csv.join('\n');
+            // Download it
+            var filename = 'export_' + table_id + '_' + new Date().toLocaleDateString() + '.csv';
+            var link = document.createElement('a');
+            link.style.display = 'none';
+            link.setAttribute('target', '_blank');
+            link.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(csv_string));
+            link.setAttribute('download', filename);
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        }
     },
     watch: {
     },
     computed: {
     },
     mounted() {
+        if(this.start !== undefined){
+            this.reportForm.inputStartDate = this.start;
+        }
+        if(this.end !== undefined){
+            this.reportForm.inputEndDate = this.end;
+        }
     }
 }
 </script>
