a33cd5cd04cc39acc61b18ec3b94da9d1890ef5f
Marsh, Kal AEST:EX <kal.marsh@gov.bc.ca>
2022-08-04 11:34:06 -0700

    adds favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,7 +12,8 @@
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-
+        <link rel="icon" href="/images/favicon.ico" type="image/vnd.microsoft.icon">
+        <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
 
         <!-- Scripts -->
         @routes
