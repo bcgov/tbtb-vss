d78e1a4c6894d72658b6feb9981627f4fef86c6b
Marsh, Kal AEST:EX <kal.marsh@gov.bc.ca>
2022-08-08 16:19:25 -0700

    updates routes to use prefix

--- routes/web.php
+++ routes/web.php
@@ -14,55 +14,59 @@ use Inertia\Inertia;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/logout', [App\Http\Controllers\UserController::class, 'logout'])->name('manual-logout');
+Route::group(['prefix' => 'oc_tbtb_vss'], function(){
 
-Route::get('/', function () {
-    return redirect('/login');
-});
+    Route::get('/logout', [App\Http\Controllers\UserController::class, 'logout'])->name('manual-logout');
 
-Route::middleware(['auth', 'active'])->group(function () {
-    Route::get('/fetch-active-users', [App\Http\Controllers\UserController::class, 'activeUsers'])->name('fetch-active-users');
-    Route::get('/fetch-cancelled-users', [App\Http\Controllers\UserController::class, 'cancelledUsers'])->name('fetch-cancelled-users');
-
-    Route::get('/dashboard', [App\Http\Controllers\IncidentController::class, 'dashboard'])->name('dashboard');
-
-    Route::resource('cases', App\Http\Controllers\IncidentController::class);
-
-    Route::get('/cases-search', function () {
-        return Inertia::render('@/Pages/CasesSearch');
-    })->name('cases-search');
-    Route::post('/case-search/sin', [App\Http\Controllers\IncidentController::class, 'sinSearch'])->name('case-sin-search');
-    Route::post('/case-search/name', [App\Http\Controllers\IncidentController::class, 'nameSearch'])->name('case-name-search');
-    Route::post('/case-search/active_user', [App\Http\Controllers\IncidentController::class, 'activeUserSearch'])->name('case-active-user-search');
-    Route::post('/case-search/cancelled_user', [App\Http\Controllers\IncidentController::class, 'cancelledUserSearch'])->name('case-cancelled-user-search');
-
-    Route::get('/case-search/{x?}', function () {
-        return Redirect::route('dashboard');
-    })->name('case-sin-search-dashboard-redirect');
-
-    Route::resource('case-funding', App\Http\Controllers\CaseFundingController::class);
-
-    Route::post('/cases/{case}/delete-sanction', [App\Http\Controllers\CaseSanctionTypeController::class, 'deleteSanction'])->name('case-funding-delete-sanction');
-    Route::post('/cases/{case}/delete-offence', [App\Http\Controllers\CaseNatureOffenceController::class, 'deleteOffence'])->name('case-funding-delete-offence');
-    Route::post('/cases/{case}/delete-area-of-audit', [App\Http\Controllers\CaseAuditTypeController::class, 'deleteAuditType'])->name('case-funding-delete-audit-type');
-
-    Route::resource('case-comment', App\Http\Controllers\CaseCommentController::class);
-
-    //authenticated admin routes
-    Route::group(['middleware' => ['admin']], function () {
-        Route::get('/reports', [App\Http\Controllers\UserController::class, 'reports'])->name('reports');
-        Route::get('/reports/download/{case}', [App\Http\Controllers\ReportController::class, 'downloadSingleStudentReport'])->name('download-single-student-report');
-//        Route::get('/reports/over_award', [App\Http\Controllers\ReportController::class, 'showOverAward'])->name('show-overaward-report');
-
-        Route::post('/reports', [App\Http\Controllers\ReportController::class, 'searchReports'])->name('reports-search');
-
-        Route::get('/maintenance', [App\Http\Controllers\MaintenanceController::class, 'goToPage'])->name('maintenance');
-        Route::get('/maintenance/staff', [App\Http\Controllers\MaintenanceController::class, 'staffList'])->name('maintenance.staff.list');
-        Route::get('/maintenance/staff/{user}', [App\Http\Controllers\MaintenanceController::class, 'staffShow'])->name('maintenance.staff.show');
-        Route::post('/maintenance/staff/{user}', [App\Http\Controllers\MaintenanceController::class, 'staffEdit'])->name('maintenance.staff.edit');
-
-        Route::get('/archive', [App\Http\Controllers\UserController::class, 'reports'])->name('archive');
-        Route::get('/archive/cases', [App\Http\Controllers\IncidentController::class, 'archived'])->name('archive.cases.list');
+    Route::get('/', function () {
+        return redirect('/login');
     });
+
+    Route::middleware(['auth', 'active'])->group(function () {
+        Route::get('/fetch-active-users', [App\Http\Controllers\UserController::class, 'activeUsers'])->name('fetch-active-users');
+        Route::get('/fetch-cancelled-users', [App\Http\Controllers\UserController::class, 'cancelledUsers'])->name('fetch-cancelled-users');
+
+        Route::get('/dashboard', [App\Http\Controllers\IncidentController::class, 'dashboard'])->name('dashboard');
+
+        Route::resource('cases', App\Http\Controllers\IncidentController::class);
+
+        Route::get('/cases-search', function () {
+            return Inertia::render('@/Pages/CasesSearch');
+        })->name('cases-search');
+        Route::post('/case-search/sin', [App\Http\Controllers\IncidentController::class, 'sinSearch'])->name('case-sin-search');
+        Route::post('/case-search/name', [App\Http\Controllers\IncidentController::class, 'nameSearch'])->name('case-name-search');
+        Route::post('/case-search/active_user', [App\Http\Controllers\IncidentController::class, 'activeUserSearch'])->name('case-active-user-search');
+        Route::post('/case-search/cancelled_user', [App\Http\Controllers\IncidentController::class, 'cancelledUserSearch'])->name('case-cancelled-user-search');
+
+        Route::get('/case-search/{x?}', function () {
+            return Redirect::route('dashboard');
+        })->name('case-sin-search-dashboard-redirect');
+
+        Route::resource('case-funding', App\Http\Controllers\CaseFundingController::class);
+
+        Route::post('/cases/{case}/delete-sanction', [App\Http\Controllers\CaseSanctionTypeController::class, 'deleteSanction'])->name('case-funding-delete-sanction');
+        Route::post('/cases/{case}/delete-offence', [App\Http\Controllers\CaseNatureOffenceController::class, 'deleteOffence'])->name('case-funding-delete-offence');
+        Route::post('/cases/{case}/delete-area-of-audit', [App\Http\Controllers\CaseAuditTypeController::class, 'deleteAuditType'])->name('case-funding-delete-audit-type');
+
+        Route::resource('case-comment', App\Http\Controllers\CaseCommentController::class);
+
+        //authenticated admin routes
+        Route::group(['middleware' => ['admin']], function () {
+            Route::get('/reports', [App\Http\Controllers\UserController::class, 'reports'])->name('reports');
+            Route::get('/reports/download/{case}', [App\Http\Controllers\ReportController::class, 'downloadSingleStudentReport'])->name('download-single-student-report');
+    //        Route::get('/reports/over_award', [App\Http\Controllers\ReportController::class, 'showOverAward'])->name('show-overaward-report');
+
+            Route::post('/reports', [App\Http\Controllers\ReportController::class, 'searchReports'])->name('reports-search');
+
+            Route::get('/maintenance', [App\Http\Controllers\MaintenanceController::class, 'goToPage'])->name('maintenance');
+            Route::get('/maintenance/staff', [App\Http\Controllers\MaintenanceController::class, 'staffList'])->name('maintenance.staff.list');
+            Route::get('/maintenance/staff/{user}', [App\Http\Controllers\MaintenanceController::class, 'staffShow'])->name('maintenance.staff.show');
+            Route::post('/maintenance/staff/{user}', [App\Http\Controllers\MaintenanceController::class, 'staffEdit'])->name('maintenance.staff.edit');
+
+            Route::get('/archive', [App\Http\Controllers\UserController::class, 'reports'])->name('archive');
+            Route::get('/archive/cases', [App\Http\Controllers\IncidentController::class, 'archived'])->name('archive.cases.list');
+        });
+    });
+    require __DIR__.'/auth.php';
+
 });
-require __DIR__.'/auth.php';
