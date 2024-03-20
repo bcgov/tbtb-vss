2a7b89b0626418e0c044399da8ce14ebb8a302d6
Kal Marsh <kal.marsh@gov.bc.ca>
2024-03-20 13:19:17 -0700

    adds sdp2

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,8 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .copyDirectory('resources/images', 'public/images')
-
     .vue()
     .postCss('resources/css/app.css', 'public/css', [require('tailwindcss'), require('autoprefixer')])
     .alias({
